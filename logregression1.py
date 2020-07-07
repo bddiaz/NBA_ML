65a6bd6bebd8438119e4eb08390d614d2e0e361a
Bryan Diaz <bddiaz@middlebury.edu>
2020-07-07 13:38:13 -0500

    commented log regressino file

--- logregression1.py
+++ logregression1.py
@@ -7,9 +7,11 @@ with the first dataset
 
 import numpy as np
 import csv
-import math
 
-
+""" getMatrix gathers the data from the csv fill and returns 
+    a list with the appropiate train dataset and test dataset.
+    
+"""
 
 def getMatrix():
     totalResults =[]
@@ -36,7 +38,8 @@ def getMatrix():
     
     with open('semifinaldataset.csv','r') as csv_file:
         csv_reader = csv.DictReader(csv_file)
-        #get all input features in large list inputFeatures
+        # get all input features and results in large lists to then
+        # be used to make numpy arrays
         for line in csv_reader:
             if line['result']== 'W':
                 totalResults.append(1)
@@ -52,7 +55,7 @@ def getMatrix():
                 temp =[]
            
         
-                
+    # gathers train dataset features            
     for feature in inputFeatures[::-1]:
         if trainMax > 0:
             trainInputFeatures.append(feature)
@@ -60,19 +63,21 @@ def getMatrix():
     
     trainMax =409
                   
-        
+    # gathers train dataset results
     for result in totalResults[::-1]:
         if trainMax > 0:
             trainResults.append(result)
             trainMax -= 1
     
-    
+    # gathers test dataset results
     count =0
     for result in totalResults[::-1]:
         if count > 408:
             testResults.append(result)
         count+=1
         
+        
+    # gathers test dataset features   
     count =0
     for feature in inputFeatures[::-1]:
         if count > 408:
@@ -82,6 +87,7 @@ def getMatrix():
     
     trainRAInputFeatures = np.array(rollingAverages(trainInputFeatures), dtype=np.float16)
     matrices.append(trainRAInputFeatures)
+    
     #trainNRAInputFeatures = np.array(trainInputFeatures)
     #matrices.append(trainNRAInputFeatures)
     
@@ -91,6 +97,10 @@ def getMatrix():
     testRAInputFeatures = np.array(rollingAverages(testInputFeatures), dtype =np.float16)
     matrices.append(testRAInputFeatures)
     
+    #testNRAInputFeatures = np.array(testInputFeatures)
+    #matrices.append(testNRAInputFeatures)
+    
+    
     testResultsVector = np.array(testResults,dtype=np.float16)
     matrices.append(testResultsVector)
     
@@ -98,7 +108,24 @@ def getMatrix():
     
     return matrices
     
+""" Rolling averages function takes in game data for a certain period, 
+    converts all the values so that they instead become the average value
+    of that position up till that game, hence the 'rolling averages' name
     
+    This is one of the ways I tested my test dataset.
+    This is because the algorithm might simply learn to add up all the scores
+    and when given new data about an individual game, then it will just add all
+    scores values and that be the prediction.
+    
+    I learned that using rolling averages that this can be used to generally get 
+    a sense of how a team does over time. With the lakers, we can see that generally 
+    they havent won much (as the guesses are pretty low), but with generally keep 
+    increasing per game (as the lakers have improved)
+    
+
+"""
+
+
 def rollingAverages(features):
     
     RAList = []
@@ -122,17 +149,31 @@ def rollingAverages(features):
     return RAList
     
     
+
+""" Sigmoid function used to bring value of hypothesis to be between
+    0 and 1. sigmoid(x) = 1/(1+e^-x)
+"""
 def sigmoid(x):
     answer =1/(1+ np.exp(-1*x))
-    #print(answer)
     return answer
 
+
+""" Hypothesis function to calculate the expected values
+    given the current thetas and training examples. 
+"""
 def hypothesis(X, Theta):
     return sigmoid(np.dot(Theta.transpose(),X))
 
 
+
+""" Gradient descent algorithm for updating theta by 
+    taking derivative of the cost function, and using it
+    to change theta to minimize the cost function. 
+
+"""
+
 def gradientDescent(Theta, X, Y, alpha):
-    print(costFn(Theta,X,Y))
+    
     for m in range(10000):
         A = hypothesis(X,Theta)
         dif = np.subtract(A,Y)
@@ -144,16 +185,21 @@ def gradientDescent(Theta, X, Y, alpha):
     
         
         
-    
+
+
+""" cost function (j(theta)) whihch is
+    which uses hypothesis and current theta to tell us the given cost
+"""
 def costFn(Theta,X,Y):
     cost = (1/409)*(-1*np.dot(Y,np.log10(hypothesis(X,Theta))) - np.dot((1-Y),np.log10(1-hypothesis(X,Theta))))
     return cost
     
 
 
+""" function tests accuracy of logistic regression algorithm results
+    and tells us accuracy.
+"""
 def testTheta(Theta,testX,testY):
-    print('new cost for test set: ')
-    print(costFn(Theta,testX,testY))
     guesses = hypothesis(testX,Theta)
     print(guesses)
     tright=0
@@ -162,7 +208,7 @@ def testTheta(Theta,testX,testY):
     for i in range(223):
         if guesses[i] <= .33:
             guesses[i] =0
-        if guesses[i]>.33:
+        if guesses[i]> .33:
             guesses[i] =1
             
     for i in range(223):
@@ -175,8 +221,6 @@ def testTheta(Theta,testX,testY):
     for i in range(223):
         if testY[i]==1:
             wCount +=1
-    print('win/loss pct: '+ str(wCount/223))
-            
     print(testY)
     print(guesses)
     percRight  = tright/(tright+twrong)
@@ -185,14 +229,18 @@ def testTheta(Theta,testX,testY):
     
     
 
-    
+""" Main function that gathers the right data and executes log regression"""
 def mainAlgorithm(): 
+    #calls getMatrix to get the train data/results and test data/results
     ms = getMatrix()
     #this X holds all training exmaples in 410x36 matrix
     Theta = np.zeros(37,)
+    #create thetas for all features
     tX = ms[0].transpose()
+    #add x0 =1 for all rows 
     x0 = np.ones(409,)
     X = np.insert(tX, 0, x0,0)
+    #grab train results
     Y = ms[1]
     
     tx0 = np.ones(223,)
